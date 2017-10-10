9aa86415049554e0f4d2cc0f079d1e2535ae8f7a
Daniels <Daniels.James@mayo.edu>
2017-10-10 17:10:40 -0500

    Uses a service to get the greeting from a different source.

---
+++ src/OdeToFood/Greeter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood
+{
+    // writing an interface to define the capabilities of every greeter
+    public interface IGreeter
+    {
+        // every greeter will have the method "GetGreeting()"
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        // intellisense generated this from squigglies at first calling
+        private string _greeting;
+
+        // this constructor demands that the user passin in an IConfiguration
+        public Greeter(IConfiguration configuration)
+        {
+            // Private field 
+            _greeting = configuration["greeting"];
+        }
+
+        // this was auto implemented with intellisense
+        public string GetGreeting()
+        {
+            // replace hard-code with greeting from 
+            return _greeting;
+        }
+    }
+}
--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -9,31 +9,47 @@ namespace OdeToFood
 {
     public class Startup
     {
-        //ctor to add a constructor for this class
-        // bringing in IHositnEnvironment to help with root path
         public Startup(IHostingEnvironment env)
         {
-            // the ConfigurationBuilder allows you to define the configuration sources for the application.
-            // we're using AddJsonFile so it can extend its configurations.
             var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath) // inorder to get this needed 2 add a dependency ...FileExtensions
+                .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables();
-            // this says "Build up the configuration that we're using"
 
             Configuration = builder.Build();
         }
-        // this will store all the configurations
+        
         public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // in order to get the iGreter working, it needs to be registered here.
+            // by defualt there are already 16 services running before we get to this point
+            // .AddSingleton() adds the service once to be used throughout.
+            // there are other adds, .AddTransient() adds a service that only lasts a little while
+
+            // this says:
+            // "whenever you see something that needs an IGreeter, 
+            // this is the concrete class that you need to instatiate and pass in as the IGreeter.
+            // this is the only part of the app that knows what kind of IGreeter we're using. 
+            services.AddSingleton<IGreeter, Greeter>();
+
+            // with this services, ,NET knows that when an IConfiguration is needed, it will pass in "Configuration"
+            // since Configuration is an Iconfiguration.
+            services.AddSingleton(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IGreeter greeter) // the configure method now also takes an IGreeter
+            // in order for it to work, it needs to be registered
+            // where are the other parameters coming from?
+            // ASP.NET knows about these services by default
         {
             loggerFactory.AddConsole();
 
@@ -44,7 +60,8 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                var greeting = Configuration["greeting"];
+                // changing it so the greeting is coming from the IGreeter instead of hard-code.
+                var greeting = greeter.GetGreeting();
                 await context.Response.WriteAsync(greeting);
             });
         }
