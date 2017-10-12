3a6063bd3a691d06a6dc073855cab8fe03d0d922
Daniels <Daniels.James@mayo.edu>
2017-10-12 16:18:25 -0500

    Demonstrates what the exception handler middleware does.

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -31,7 +31,6 @@ namespace OdeToFood
 
         // This method gets called by the runtime. 
         // Use this method to configure the HTTP request pipeline.
-        // all HTtP requests go through this middleware
         public void Configure(
             IApplicationBuilder app, 
             IHostingEnvironment env, 
@@ -40,39 +39,31 @@ namespace OdeToFood
         {
             loggerFactory.AddConsole();
 
-            // this middleware displays an error pages that is more useful for developers.
-            // it only activates when you're in developer mode.
+            /*
+             * Again, placement is key. This block has to be here because middleware is run from top to bottom,
+             * then bottom to top on its way back out
+             * |   A
+             * |   |
+             * V---|
+             */
+
             if (env.IsDevelopment())
             {
-                // for demo, commented out ASP.NET core diagnostics dependency
-                // w/o it, the next line errored, not many pieces of middleware available
+                // commenting out for demo
+                // with this middleware, unhandled exceptions are given a nice page the user can see.
+                // it has a stack trace and shows 
                 app.UseDeveloperExceptionPage();
             }
 
-            // added this middleware from Diagnostics 
-            // note: before app.Run
-            // serves a basic welcome page, useful for debugging, does this app serve anything?
-            // Terminal piece of middleware when invoked without parameters. No other middleware can be reached
-            //app.UseWelcomePage();
-
-            // This overload says, "handle all requests to the root url/welcome
-            //app.UseWelcomePage("/welcome");
-
-            // there is this property that you can run on a WelcomePage that allows one to add options.
-            // most middleware will have an object like this. It will usually follow the convetion of 
-            // MiddlwareNameOptions
             app.UseWelcomePage(new WelcomePageOptions
             {
-                Path = "/welcomer"
+                Path = "/welcome"
             });
 
-            // dont' normally do much with .Run. More for demo purposes, it's low-level.
             app.Run(async (context) =>
             {
-               // All HTTP req go through this w/ current setup.
-               // .NET gives the context object, which has all kinds of information about the req
-               // e.g. context.Request.body
-               
+                // This is just an unhandled exception to help demonstrate what app.UseDeveloperExceptionPage does
+                throw new System.Exception("Something went wronmg");
                 var greeting = greeter.GetGreeting();
                 await context.Response.WriteAsync(greeting);
             });
