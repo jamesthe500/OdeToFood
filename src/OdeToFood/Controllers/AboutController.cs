aaddced0e6a5644942ec162d2a87eb6980b6522b
Daniels <Daniels.James@mayo.edu>
2017-10-18 16:18:20 -0500

    Sophistacizes the controller.

--- src/OdeToFood/Controllers/AboutController.cs
+++ src/OdeToFood/Controllers/AboutController.cs
@@ -2,42 +2,18 @@
 
 namespace OdeToFood.Controllers
 {
-    // ATTRIBUTE ROUTING
-
-    // Route Attribute
-    // "in order to reach this controller, the first part of the url has to be 'about'
-    //[Route("about")]
-
-    // another way is with "TOKENS" these are inside the []
-    // "in order to get this route, look for the controller below, i.e. "About"
-    //[Route("[controller]")]
-
-    // a third way, even more efficient. Can include literals.
+    
     [Route("company/[controller]/[action]")]
     public class AboutController
     {
-        // ...and then the next part of the url can be empty
-        //[Route("")]
-
-        // this way, if you need to refactor, so will the route refactor
-        //[Route("[action]")]
         public string Phone()
         {
             return "(507) 284-2511";
         }
-
-        // To get this controller, you need a route like /about/address
-        //[Route("[action]")]
+        
         public string Address()
         {
             return "Rochester";
         }
-
-        /*
-        public string Index()
-        {
-            return "Pointer finger";
-        }
-        */
     }
 }
--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    // ususally, when cprogramming controllers, we derive from the base-class "Controller" of MS.AspNEtCore.Mvc
+    //  
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello, from the Homecontroller";
+            // first thing it will do is instatniate new Restaurant (had to using this namespace)
+            var model = new Restaurant
+            {
+                Id = 1,
+                Name = "Casa di Kobe"
+            };
+
+            // returning this instead of simple text for the greater flexibility, and stuff.
+            // it derives from IActionResult, thus that change above.
+            //return Content( "Hello, from the Homecontroller");
+
+            // this will serialize teh data and return it as json by default
+            // this is great for an API
+            return new ObjectResult(model);
         }        
     }
 }
