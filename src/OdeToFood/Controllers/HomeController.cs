8cc38de4630803e9f93f24961fa6dc1c12cc4d9c
Daniels <Daniels.James@mayo.edu>
2017-10-23 20:39:46 -0500

    Creates a form for creation.

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -24,32 +24,22 @@ namespace OdeToFood.Controllers
             return View(model);
         }
 
-        // this is temporarily returning a string just for testing purposes.
-        // when we browse to /home/details/4 it renders "4" 
-        // it is taking the 4 at the end of the URL as a parameter and returning it.
-        // recall that in Startup.cs we have the default route "{controller=home}/{action=Index}/{id?}"
-        // so that third part of the path is already known as "id"
-        // this also works for a query string, i.e. /home/details?id=5
-        // in this case, we get 4. It favors routing data over query string /home/details/4?id=5
         public IActionResult Details(int id)
         {
             var model = _restaurantData.Get(id);
-            // other ways to handle the null possiblity
+          
             if (model == null)
             {
-                // this one isn't so great . It's just a 404 page
-                //return NotFound();
-
-                // redirects are a better approach, and there are many to choose from.
-                // will send it to that other action result. 
-                // the "nameof" method makes refactoring later on handy.
-                // this sends the user back to the home page
                 return RedirectToAction(nameof(Index));
-
             }
 
-            // we'll return this view, which warrants creating one...
             return View(model);
+        }
+
+        // This is all that is needed to create a view of a restaurant creation page
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
--- src/OdeToFood/Entities/Restaurant.cs
+++ src/OdeToFood/Entities/Restaurant.cs
@@ -1,11 +1,20 @@
-﻿// changed the folder from Models to Entities.
-// changed the namespace to follow the documetn structure.
-// Distinction is that an entity is somethign you persist in the database?
-namespace OdeToFood.Entities
+﻿namespace OdeToFood.Entities
 {
+    // adding the enum up here because it's not a simple string or int
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        French,
+        Japanese,
+        American,
+        Sushi
+    }
+
     public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }
